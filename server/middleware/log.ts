1f5e1ddc0057209cedcc723b1518103d3d753ed6
Samuele Musiani <samu@teapot.ovh>
2024-09-18 17:57:14 +0200

    add loggin middleware

---
+++ server/middleware/log.ts
@@ -0,0 +1,9 @@
+export default defineEventHandler((event) => {
+  let ip = getRequestIP(event) ?? ""
+  //console.log(ip + getRequestURL(event))
+
+  const method = event.node.req.method
+  const path = event.node.req.url
+  const d = new Date().toLocaleTimeString()
+  console.log(`[${d}] ${method} ${path}`)
+})
