0fdff8dfbdb92bc146c907d34ee26a42cf2e232a
Samuele Musiani <samu@teapot.ovh>
2024-12-25 23:12:48 +0100

    update projects tasks state based on tm

--- server/api/tm.get.ts
+++ server/api/tm.get.ts
@@ -1,10 +1,3 @@
 export default defineEventHandler(async (_): Promise<number> => {
-  let delta = (await useStorage().getItem(`tm:delta`)) as number | null
-
-  if (delta === null) {
-    console.error('tm:delta is NULL')
-    delta = 0
-  }
-
-  return Date.now() + delta
+  return await getNowTime()
 })
