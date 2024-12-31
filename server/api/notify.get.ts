b9d641d3ce4fd9c4e7c6234b29e99d4b8531f210
Samuele Musiani <samu@teapot.ovh>
2025-01-01 00:16:06 +0100

    refactor push notifications

--- server/api/notify.get.ts
+++ server/api/notify.get.ts
@@ -1,33 +1,6 @@
-import webpush from 'web-push'
-import { PushNotification } from '@/server/db'
-
 export default defineEventHandler(async (event) => {
   try {
-    const options = await PushNotification.find({
-      user_id: event.context.auth.id
-    })
-
-    if (!options) {
-      throw new Error(
-        'Options for notify are empty for user: ',
-        event.context.auth.id
-      )
-    }
-
-    for (const opt of options) {
-      try {
-        await webpush.sendNotification(
-          opt.subscription,
-          JSON.stringify({
-            title: 'Test notification',
-            body: 'This is a test from the backend'
-          })
-        )
-      } catch (err) {
-        // Avoid errors when notifictions options are old and webpush
-        // returns "Received unexpected response code"
-      }
-    }
+    sendPushNotification(event.context.auth.id)
   } catch (err) {
     console.error(err)
     setResponseStatus(event, 500)
--- server/utils/notifications.ts
+++ server/utils/notifications.ts
@@ -1,5 +1,7 @@
 import { User, Notification } from '@/server/db'
 import { getIo } from '@/server/plugins/02.socket.io'
+import webpush from 'web-push'
+import { PushNotification } from '@/server/db'
 
 // Type can be 'basic', 'project-invited', 'event-invited'.
 // indetifier is the id of the project or event for the invitation.
@@ -29,10 +31,42 @@ export async function sendNotification(
       user_id: user.id
     })
 
-    await notification.save()
     console.log('Sending notification to: ', user.id.toString())
+
+    // Internal site notification
+    await notification.save()
+
+    // Websocket notification
     getIo().to(user.id.toString()).emit('notification', notification)
+
+    // Push notification
+    await sendPushNotification(user.id.toString())
   } catch (e) {
     console.error('Saving notification: ', e)
+  }
+}
+
+export async function sendPushNotification(id: string) {
+  const options = await PushNotification.find({
+    user_id: id
+  })
+
+  if (!options) {
+    throw new Error('Options for notify are empty for user: ' + id)
+  }
+
+  for (const opt of options) {
+    try {
+      await webpush.sendNotification(
+        opt.subscription,
+        JSON.stringify({
+          title: 'Test notification',
+          body: 'This is a test from the backend'
+        })
+      )
+    } catch (err) {
+      // Avoid errors when notifictions options are old and webpush
+      // returns "Received unexpected response code"
+    }
   }
 }
