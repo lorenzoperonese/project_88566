124d3020c9140cb29a67ca4718683b522b9bb29c
Samuele Musiani <samu@teapot.ovh>
2025-01-07 13:13:23 +0100

    improvements on toasts

--- plugins/toast.client.ts
+++ plugins/toast.client.ts
@@ -2,10 +2,22 @@ import * as vt from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
 
 const options: vt.PluginOptions = {
-  timeout: 5000,
+  timeout: 3000,
   position: vt.POSITION.TOP_RIGHT,
   maxToasts: 5,
-  showCloseButtonOnHover: true
+  showCloseButtonOnHover: true,
+  closeOnClick: true,
+  pauseOnHover: false,
+  hideProgressBar: true,
+  transition: 'Vue-Toastification__fade',
+  filterBeforeCreate: (toast, toasts) => {
+    if (toasts.filter((t) => t.type === toast.type).length !== 0) {
+      // Returning false discards the toast
+      return false
+    }
+    // You can modify the toast if you want
+    return toast
+  }
 }
 
 export default defineNuxtPlugin((nuxtApp) => {
